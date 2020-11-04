c55f9af60ad893d3c746d7188c1082c9956f188e
Marc <marcllopis89@gmail.com>
2020-11-04 19:58:10 +0100

    Adds the whole game logic

--- src/App.js
+++ src/App.js
@@ -2,25 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 import GameCapitals from './gameCapitals'
+import { shuffle } from './functions'
+import './App.css'
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
 
 class App extends Component {
 
@@ -29,7 +13,7 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const {data} = await axios('https://restcountries.eu/rest/v2/all');
+    const { data } = await axios('https://restcountries.eu/rest/v2/all');
     let dataFiltered = data.filter(country => country.capital !== '')
 
 
@@ -39,9 +23,11 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Wild Capitals</h1>
-        <GameCapitals countries={this.state.data} />
+      <div className='container'>
+        <div className='instructions'>
+          <h1>Wild Capitals</h1>
+          <GameCapitals countries={this.state.data} />
+        </div>
       </div>
     );
   }
--- src/gameCapitals.js
+++ src/gameCapitals.js
@@ -1,23 +1,6 @@
 import React, { Component } from 'react';
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
+import { shuffle } from './functions'
 
 
 class gameCapitals extends Component {
@@ -27,7 +10,8 @@ class gameCapitals extends Component {
     countries: [],
     fourCapitals: [],
     points: 0,
-    incorrect: false
+    incorrect: false,
+    seconds: 30
   }
 
   startGame = () => {
@@ -37,6 +21,21 @@ class gameCapitals extends Component {
     let fourCapitals = [arrayCountries[0].capital, arrayCountries[1].capital, arrayCountries[2].capital, arrayCountries[3].capital]
 
     shuffle(fourCapitals)
+
+    const countdown = () => {
+      console.log(this.state.seconds)
+      if (this.state.seconds <= 0) {
+        this.setState({
+          gameStatus: 'gameOver'
+        })
+        clearInterval(timerId)
+      }
+      this.setState({
+        seconds: this.state.seconds - 1
+      })
+    }
+
+    let timerId = setInterval(countdown, 1000)
 
     this.setState({
       gameStatus: 'playingGame',
@@ -60,23 +59,35 @@ class gameCapitals extends Component {
         countries: newCountries,
         fourCapitals: newFourCapitals,
         points: this.state.points + 10,
+        seconds: this.state.seconds + 4,
         incorrect: false
       })
 
     } else {
       console.log('wrong')
-      if (this.state.points -2 < 0){
+      if (this.state.points - 2 < 0) {
         this.setState({
           points: 0,
+          seconds: this.state.seconds - 2,
           incorrect: true
         })
       } else {
         this.setState({
           points: this.state.points - 2,
+          seconds: this.state.seconds - 2,
           incorrect: true
         })
       }
     }
+  }
+
+  playAgain = () => {
+    this.setState({
+      gameStatus: 'startGame',
+      points: 0,
+      incorrect: false,
+      seconds: 30
+    })
   }
 
   render() {
@@ -93,14 +104,25 @@ class gameCapitals extends Component {
         {
           this.state.gameStatus === 'playingGame' &&
           <div>
+            <h2>Current points: {this.state.points} || Time Left: {this.state.seconds}</h2>
             <h1>{this.state.countries[0].name}</h1>
-            <h2>Current points: {this.state.points}</h2>
             <h3>{this.state.incorrect && 'WRONG!!'}</h3>
-            {
-              this.state.fourCapitals.map((capital, index) => (
-                <button key={index} id={capital} onClick={this.chooseCapital}>{capital}</button>
-              ))
-            }
+            <div className='button-container'>
+              {
+                this.state.fourCapitals.map((capital, index) => (
+                  <button key={index} id={capital} onClick={this.chooseCapital}>{capital}</button>
+                ))
+              }
+            </div>
+          </div>
+        }
+        {
+          this.state.gameStatus === 'gameOver' &&
+          <div>
+            <h1>Time runned out!</h1>
+            <p>You did {this.state.points} points!</p>
+            <p>Do you want to try it again?</p>
+            <button onClick={this.playAgain}>Play again</button>
           </div>
         }
       </div>
